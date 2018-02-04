0bbb2fba1a7d4910c0fdb90493a36247128918a1
Isopach <Isopach@users.noreply.github.com>
2018-02-05 03:54:02 +0900

    Create reverse-bits.py

---
+++ Python/reverse-bits.py
@@ -0,0 +1,9 @@
+#190. Reverse Bits
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        n = '{0:032b}'.format(n)
+        n = n[::-1]
+        return int(n,2)
