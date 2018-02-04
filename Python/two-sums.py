fbb0213fe376405898b749c70a0f1ab84c6b9c09
Isopach <Isopach@users.noreply.github.com>
2018-02-05 05:07:59 +0900

    Add Two-pass Hash Approach

--- Python/two-sums.py
+++ Python/two-sums.py
@@ -16,4 +16,23 @@ class Solution(object):
                 search[num] = i                 #repeat search for next element
                 
 
-#
+#Two-pass Hash Table
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ndict = {k: v for v, k in enumerate(nums)}              #Creates a dictionary with element as key and index as value
+        r = [0,0]
+        for i, num in enumerate(nums):                          #loops through input array
+            complement = target - nums[i]                       #initializes complement and defines it as target - current element
+            if complement in ndict and ndict[complement] != i:  #if the complement exists and does not equal to current element nums[i]
+                r[0] = ndict[complement]                        #sets first element in result to value of complement in ndict i.e. the index
+                r[1] = i                                        #sets second element in result to current index 
+        return r
+
+    
+#One-pass Hash Table
